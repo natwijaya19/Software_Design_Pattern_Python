6944a1fdb3cbce12d14bad7c73f20850c7e99e4c
natwijaya19 <natwijaya19@gmail.com>
2022-12-05 18:36:31 +0700

    Visitor pattern update

--- design_patterns/Visitor_Pattern/Visitor_Pattern1.py
+++ design_patterns/Visitor_Pattern/Visitor_Pattern1.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-
 """
 Visitor is a behavioral design pattern that lets you separate algorithms from the objects on which they operate.
 """
@@ -8,12 +7,14 @@ Visitor is a behavioral design pattern that lets you separate algorithms from th
 from abc import ABC, abstractmethod
 from typing import List
 
+
 # create component class interface
 class ComponentInterface(ABC):
     """
     The Component interface declares an `accept` method that should take the
     base visitor interface as an argument.
     """
+
     @abstractmethod
     def accept(self, visitor: VisitorInterface) -> None:
         """
@@ -21,25 +22,28 @@ class ComponentInterface(ABC):
         """
         pass
 
+
 # create concrete component class
 class ConcreteComponentA(ComponentInterface):
     """
     Each Concrete Component must implement the `accept` method in such a way
     that it calls the visitor's method corresponding to the component's class.
     """
+
     def accept(self, visitor_object: VisitorInterface) -> None:
         """
         Note that we're calling `visitConcreteComponentA`, which matches the
         current class name. This way we let the visitor know the class of the
         component it works with.
 
-        :param visitor_object:
-        :return:None
+        - param visitor_object:
+        - return:None
         """
 
         visitor_object.visit_concrete_component_a(self)
 
-    def exclusive_method_of_concrete_component_a(self) -> str:
+    @staticmethod
+    def exclusive_method_of_concrete_component_a() -> str:
         """
         Concrete Components may have special methods that don't exist in their
         base class or interface. The Visitor is still able to use these methods
@@ -48,14 +52,17 @@ class ConcreteComponentA(ComponentInterface):
         """
         return "A"
 
+
 class ConcreteComponentB:
     """
     Same here: visitConcreteComponentB => ConcreteComponentB
     """
+
     def accept(self, visitor_object: VisitorInterface) -> None:
         visitor_object.visit_concrete_component_b(self)
 
-    def exclusive_method_of_concrete_component_b(self) -> str:
+    @staticmethod
+    def exclusive_method_of_concrete_component_b() -> str:
         return "B"
 
 
@@ -66,6 +73,7 @@ class VisitorInterface(ABC):
     component classes. The signature of a visiting method allows the visitor to
     identify the exact class of the component that it's dealing with.
     """
+
     @abstractmethod
     def visit_concrete_component_a(self, element: ConcreteComponentA) -> None:
         """
@@ -79,6 +87,7 @@ class VisitorInterface(ABC):
         """
         pass
 
+
 """
 Concrete Visitors implement several versions of the same algorithm, which can
 work with all concrete component classes.
@@ -88,6 +97,7 @@ a complex object structure, such as a Composite tree. In this case, it might be
 helpful to store some intermediate state of the algorithm while executing
 visitor's methods over various objects of the structure.
 """
+
 
 # create concrete visitor classes
 class ConcreteVisitor1(VisitorInterface):
@@ -106,19 +116,20 @@ class ConcreteVisitor2(VisitorInterface):
         print(f"{element.exclusive_method_of_concrete_component_b()} + ConcreteVisitor2")
 
 
-def client_code(components: List[ComponentInterface], visitor: VisitorInterface) -> None:
+def client_code(components_list: List[ComponentInterface], visitor: VisitorInterface) -> None:
     """
     The client code can run visitor operations over any set of elements without
     figuring out their concrete classes. The accept operation directs a call to
     the appropriate operation in the visitor object.
-    :param components:
-    :param visitor:
-    :return: None
+    - param components_list:
+    - param visitor:
+    - return: None
     """
-    for component in components:
+    for component in components_list:
         component.accept(visitor)
 
     # ...
+
 
 if __name__ == "__main__":
     components = [ConcreteComponentA(), ConcreteComponentB()]
