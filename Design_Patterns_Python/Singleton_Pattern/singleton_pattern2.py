a99e781912af5c1f9fe6b833de1cced6b7ca50e1
natwijaya19 <natwijaya19@gmail.com>
2022-12-05 20:59:11 +0700

    Singleton pattern update - thread safe implementation

--- Design_Patterns_Python/Singleton_Pattern/singleton_pattern2.py
+++ Design_Patterns_Python/Singleton_Pattern/singleton_pattern2.py
@@ -15,4 +15,82 @@ Most of the time, this limitation comes up during the creation of unit tests.
 
 from threading import Lock, Thread
 
+class SingletonMetha(type):
+    """
+    This is a thread-safe implementation of Singleton.
+    """
+    _instances = {}
+    _lock: Lock = Lock()
+    """
+    We now have a lock object that will be used to synchronize threads during
+    first access to the Singleton.
+    """
+
+    def __call__(cls, *args, **kwargs):
+        """
+        Possible changes to the value of the `__init__` argument do not affect
+        the returned instance.
+
+        Now, imagine that the program has just been launched. Since there's no
+        Singleton instance yet, multiple threads can simultaneously pass the
+        previous conditional and reach this point almost at the same time. The
+        first of them will acquire lock and will proceed further, while the
+        rest will wait here.
+        """
+        with cls._lock:
+            """
+            The first thread to acquire the lock, reaches this conditional,
+            goes inside and creates the Singleton instance. Once it leaves the
+            lock block, a thread that might have been waiting for the lock
+            release may then enter this section. But since the Singleton field
+            is already initialized, the thread won't create a new object.
+            """
+            if cls not in cls._instances:
+                cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Singleton(metaclass=SingletonMetha):
+
+    value: str = None
+
+    """
+    We'll use this property to prove that our Singleton really works.
+
+    The Singleton's constructor should always be private to prevent direct
+    construction calls with the `new` operator.
+    """
+
+    def __init__(self, value: str) -> None:
+        self.value = value
+
+    def some_business_logic(self) -> None:
+        """
+        Finally, any singleton should define some business logic, which can be
+        executed on its instance.
+        """
+        pass
+
+def test_singleton(value: str) -> None:
+    singleton = Singleton(value)
+    print(singleton.value)
+
+
+if __name__ == "__main__":
+    # the client code
+    print("If you see the same value, then singleton was reused (yay!)\n"
+          "If you see different values, "
+          "then 2 singletons were created (booo!!)\n\n"
+          "RESULT:\n")
+
+    process1 = Thread(target=test_singleton, args=("FOO",))
+    process2 = Thread(target=test_singleton, args=("BAR",))
+    process1.start()
+    process2.start()
+
+    process3 = Thread(target=test_singleton, args=("WOO",))
+    process4 = Thread(target=test_singleton, args=("HOO",))
+    process3.start()
+    process4.start()
+
+
 
