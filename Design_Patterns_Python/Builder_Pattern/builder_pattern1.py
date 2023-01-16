43f1de7a9e20cb7b46f6f1b96219d246af08bcda
natwijaya19 <72418586+natwijaya19@users.noreply.github.com>
2023-01-16 15:00:38 +0700

    Refactor builder pattern

--- Design_Patterns_Python/Builder_Pattern/builder_pattern1.py
+++ Design_Patterns_Python/Builder_Pattern/builder_pattern1.py
@@ -143,31 +143,3 @@ class Director:
         self.builder.produce_part_c()
 
 
-if __name__ == "__main__":
-    """
-    The client code creates a builder_instance object, passes it to the director and then
-    initiates the construction process. The end result is retrieved from the
-    builder_instance object.
-    """
-    director = Director()
-    builder = ConcreteBuilder1()
-    director.builder = builder
-
-    print("Standard basic product")
-    director.build_minimal_viable_product()
-    builder.product.list_parts()
-
-    print("\n")
-
-    print("standard full featured product")
-    director.build_full_featured_product()
-    builder.product.list_parts()
-
-    print("\n")
-
-    # Remember, the Builder pattern can be used without a Director class.
-    print("custom product")
-    builder.produce_part_a()
-    builder.produce_part_b()
-    builder.product.list_parts()
-
